c5c3c9dd118d51f4f1c06567c01993dcea6decce
Antonio Solis <solisga@hotmail.com>
2019-02-10 14:54:12 -0600

    Creacion de nuestra primera aplicacion en GitHub

---
+++ app/src/main/java/mx/com/solgodan/appclima/Ciudades.kt
@@ -0,0 +1,12 @@
+package mx.com.solgodan.appclima
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Ciudades : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciudades)
+    }
+}
